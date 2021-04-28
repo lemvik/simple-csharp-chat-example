6ac94d53447ffadec9599ddbf959c81052d0bb37
Victor Lemeshev <victor.lemeshev@protonmail.com>
2021-04-28 19:53:34 +0300

    Generated solution skeleton.
    - First sketch - client, server and shared protocol library.
    - Maybe there will be more shared code, the intention is to have server and client thin enough and most of the code shared.

---
+++ chat-client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Critical.Chat.Client
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Starting client.");
+        }
+    }
+}
---
+++ chat-server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Critical.Chat.Server
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Starting server.");
+        }
+    }
+}
