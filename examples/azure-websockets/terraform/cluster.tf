670ad3b1f08099c8f9e7ee65a47423d7daadc42e
Victor Lemeshev <victor.lemeshev@protonmail.com>
2021-05-15 19:26:38 +0300

    Iterating on Azure pipelines.
    - Added Docker buildAndPush task.
    - Requires some manual setup in DevOps.

--- examples/azure-websockets/terraform/cluster.tf
+++ examples/azure-websockets/terraform/cluster.tf
@@ -7,7 +7,7 @@ resource "azurerm_kubernetes_cluster" "websocket_chat_cluster" {
   default_node_pool {
     name       = "default"
     node_count = 2
-    vm_size    = "Standard_B1ms"
+    vm_size    = "Standard_B2s"
   }
 
   identity {
